697a0bb7a4e6a2c115b830b95e317cdcd5c48c43
z-x-y613 <3921892096@qq.com>
2026-09-25 14:23:56 +0800

    Replace protection book with milk bucket

--- src/main/java/com/yourname/jizoaddon/ModEvents.java
+++ src/main/java/com/yourname/jizoaddon/ModEvents.java
@@ -4,7 +4,6 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = JizoAddon.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
@@ -14,10 +13,5 @@ public class ModEvents {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.HALF_TOTEM);
         }
-    }
-
-    @SubscribeEvent
-    public static void commonSetup(FMLCommonSetupEvent event) {
-        event.enqueueWork(ProtectionBookIngredient::register);
     }
 }
